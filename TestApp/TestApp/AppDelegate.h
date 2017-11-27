14cbfda2d5f5ef8010ff0e0a9862826291f79999
Jason Liang <liangjyjason@gmail.com>
2017-11-27 10:13:44 -0500

    Init commit.

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Jason Liang on 11/27/17.
+//  Copyright © 2017 Jason Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Jason Liang on 11/27/17.
+//  Copyright © 2017 Jason Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
